772f8608d96ffd71ccc2f9003505e17b53a53c98
rgrist15 <54677458+rgrist15@users.noreply.github.com>
2020-09-14 16:35:19 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jre1.8.0_251\bin\java.exe" -Xmx4096M -Xms512M -jar server.jar nogui
+PAUSE
---
+++ start.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jre1.8.0_251\bin\java.exe" -Xmx8190M -Xms512M -jar server.jar nogui
+PAUSE
